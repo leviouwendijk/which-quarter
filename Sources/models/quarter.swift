a3ff47c6665caec43e23a5cbd7fb9127315a7601
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-04 15:55:45 +0100

    redundant =

--- Sources/models/quarter.swift
+++ Sources/models/quarter.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 enum Quarter: String, RawRepresentable, CaseIterable {
-    case q1 = "q1"
-    case q2 = "q2"
-    case q3 = "q3"
-    case q4 = "q4"
+    case q1 
+    case q2 
+    case q3 
+    case q4 
 
     var int: Int { 
         switch self {
