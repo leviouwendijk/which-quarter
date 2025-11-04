71afbf57f1b8a28a26629bf8122bfdc13e458e24
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-11-04 16:45:14 +0100

    streamline printing q

--- Sources/logic/provide-quarter.swift
+++ Sources/logic/provide-quarter.swift
@@ -2,19 +2,14 @@ import Foundation
 
 func provide_quarter(_ input: String) throws -> String {
     print("Trying quarter..")
+    print()
 
     if let q_num = Int(input), (1...4).contains(q_num) {
         let quarter = try Quarter(from: q_num)
         return try quarter.printable()
     } else {
         if let quarter = Quarter(rawValue: input.lowercased()) {
-            let ordered = try quarter.see().printable()
-            let output = """
-            \(quarter.rawValue): 
-
-            \(ordered)
-            """
-            return output
+            return try quarter.printable()
         }
     } 
 
